b025e33826db43e553f166523cce7ca21cbc5884
simota <simota@me.com>
2017-04-12 00:38:57 +0900

    WIP

--- lib/numbers.ex
+++ lib/numbers.ex
@@ -21,12 +21,37 @@ defmodule Numbers do
     resp.body
   end
 
+  def is_valid_integer(x) do
+    Integer.parse(x) != :error
+  end
+
   def parse_html(html) do
+    html
+    |> Floki.find("td.number")
+    |> Enum.map(fn(x) -> Floki.text(x) end)
+    |> Enum.filter(fn(x) -> is_valid_integer(x) end)
+  end
+
+  def preparation(url) do
+    url
+    |> fetch_html
+    |> parse_html
+  end
+
+  def sum_occurrences(data) do
+    data
+    |> Enum.group_by(fn(x) -> x end)
+    |> Enum.map(fn({k, v}) -> {k, length(v)} end)
+    |> Enum.sort(fn(a, b) -> elem(a, 1) > elem(b, 1) end)
+    |> Enum.each(fn({k,v}) -> IO.puts("#{k} => #{v}") end)
   end
 
   def main(args) do
-    url = "http://kaeru-dayo.com/numbers3/numbers3-ichiran.php?kaisuu=all"
-    html = fetch_html(url)
+    num = List.first(args)
+    url = "http://kaeru-dayo.com/numbers#{num}/numbers#{num}-ichiran.php?kaisuu=all"
+    data = preparation(url)
+    ret = sum_occurrences(data)
+    IO.inspect ret
   end
 
 end
